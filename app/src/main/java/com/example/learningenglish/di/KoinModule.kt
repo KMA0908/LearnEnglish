92d0a1f5779047d11c0cf523b7b13fc2fbce40dc
HuyTran <quanghuypn01@gmail.com>
2023-12-07 10:26:18 +0700

    edit profile 2

--- app/src/main/java/com/example/learningenglish/di/KoinModule.kt
+++ app/src/main/java/com/example/learningenglish/di/KoinModule.kt
@@ -8,6 +8,10 @@ import com.example.learningenglish.viewmodel.LoginViewModel
 import com.example.learningenglish.viewmodel.MainViewModel
 import com.example.learningenglish.viewmodel.SplashViewModel
 import com.example.learningenglish.viewmodel.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -22,5 +26,8 @@ val KoinModule = module {
         single { SQLHelper(androidContext()) }
         viewModel { ProfileViewModel() }
         viewModel { UserAccountViewModel() }
+        single { FirebaseAuth.getInstance() }
+        single { Firebase.firestore }
+        single { FirebaseStorage.getInstance().reference }
 
 }
