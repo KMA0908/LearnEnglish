ea06afd3212363646e961076100c1c53792fac20
NguyenTMinh <techit606@gmail.com>
2023-12-12 23:45:10 +0700

    them chuc nang import tu file csv

--- app/src/main/java/com/example/learningenglish/activity/AddTopicActivity.kt
+++ app/src/main/java/com/example/learningenglish/activity/AddTopicActivity.kt
@@ -1,11 +1,16 @@
 package com.example.learningenglish.activity
 
+import android.content.Intent
+import android.util.Log
 import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.learningenglish.R
+import com.example.learningenglish.adapter.WordPreviewAdapter
 import com.example.learningenglish.base.BaseActivity
 import com.example.learningenglish.database.SQLHelper
 import com.example.learningenglish.databinding.ActivityAddTopicBinding
 import com.example.learningenglish.model.Topic
+import com.example.learningenglish.model.Word
 import com.example.learningenglish.viewmodel.SplashViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -13,17 +18,21 @@ import com.google.firebase.firestore.FirebaseFirestore
 import io.reactivex.rxjava3.schedulers.Schedulers.single
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class AddTopicActivity: BaseActivity<ActivityAddTopicBinding, SplashViewModel>() {
     private val sqlHelper: SQLHelper by inject()
     companion object {
         internal val TAG = SplashActivity::class.java.name
         internal val TOPIC_COLLECTION = "topic"
+        internal val PICK_CSV_REQUEST_CODE = 1011
     }
     private val viewModel: SplashViewModel by viewModel()
     private var modeTopic = 0
     private val firestore: FirebaseFirestore by inject()
     private val firebaseAuth: FirebaseAuth by inject()
+    private lateinit var previewAdapter: WordPreviewAdapter
 
     override fun getVM(): SplashViewModel = viewModel
 
@@ -37,7 +46,11 @@ class AddTopicActivity: BaseActivity<ActivityAddTopicBinding, SplashViewModel>()
         }
         binding.tvDone.setOnClickListener {
             val topic = Topic(binding.edtNameTopic.text.toString(),0, modeTopic)
-            sqlHelper.addTopic(topic, binding.spinnerFolder.selectedItem.toString().toInt())
+            val topicId = sqlHelper.addTopic(topic, binding.spinnerFolder.selectedItem.toString().toInt())
+            // Neu pre list khong rong tuc la dang import thi save cac tu dang import vao topic nay
+            for (word in viewModel.prevListWords) {
+                sqlHelper.addWord(word, topicId.toInt())
+            }
             // Topic la public thi day len firebase
             if (topic.mode == 1) {
                 val data = hashMapOf(
@@ -64,7 +77,45 @@ class AddTopicActivity: BaseActivity<ActivityAddTopicBinding, SplashViewModel>()
         )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerFolder.adapter = adapter
+
+        // Nut import file csv
+        binding.tvImport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "text/*"
+
+            startActivityForResult(intent, PICK_CSV_REQUEST_CODE)
+        }
+
+        //
+        previewAdapter = WordPreviewAdapter(viewModel.prevListWords)
+        binding.rvPreviewImport.adapter = previewAdapter
+        binding.rvPreviewImport.layoutManager = LinearLayoutManager(this)
     }
 
     override fun getLayoutId() = R.layout.activity_add_topic
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PICK_CSV_REQUEST_CODE) {
+                data?.data?.let {
+                    val inputStream = contentResolver.openInputStream(it)
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    var line: String?
+
+                    while (reader.readLine().also { line = it } != null) {
+                        // Lấy dữ liệu từng cột trong dòng
+                        val columns = line!!.split(",")
+                        val word = Word(columns.get(0), columns.get(1), 0)
+                        viewModel.prevListWords.add(word)
+                        Log.d(TAG, "onActivityResult: $word")
+                    }
+
+                    previewAdapter.notifyDataSetChanged()
+                    reader.close()
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/learningenglish/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/example/learningenglish/viewmodel/SplashViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.learningenglish.viewmodel
 
 import com.example.learningenglish.base.BaseViewModel
+import com.example.learningenglish.model.Word
 import com.example.learningenglish.utils.SingleLiveEvent
+import java.util.ArrayList
 
 class SplashViewModel : BaseViewModel() {
     val stateSplash =  SingleLiveEvent<Boolean>()
+    val prevListWords: MutableList<Word> = mutableListOf()
+
     init {
         stateSplash.value = true
     }
